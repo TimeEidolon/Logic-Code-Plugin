36bcb4940e2ea577d211968de39c1ace7b83d64f
GitHub Action <action@github.com>
2026-05-06 03:18:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/timeeidolon/logiccodeplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/timeeidolon/logiccodeplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.timeeidolon.logiccodeplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
